6398475585747d5bce5c548f4bdcc21e0210d97d
carlosescamilla90 <carlos.escamillarodriguez@unicolombo.edu.co>
2024-11-19 01:51:27 -0500

    Taller 1 - Ejercicio 1

--- ejercicio 1.0/Libro.java
+++ ejercicio 1.0/Libro.java
@@ -7,33 +7,31 @@ public class Libro {
 
     // Constructor por defecto que asigna valores predeterminados
     public Libro() {
-        this.titulo = "Desconocido"; // Valor predeterminado para el título
-        this.autor = "Desconocido";  // Valor predeterminado para el autor
-        this.numeroPaginas = 0;      // Valor predeterminado para el número de páginas
+        this.titulo = "Desconocido"; 
+        this.autor = "Desconocido";  
+        this.numeroPaginas = 0;     
     }
 
     // Constructor parametrizado que permite asignar valores específicos
     public Libro(String titulo, String autor, int numeroPaginas) {
-        this.titulo = titulo;            // Asigna el valor del parámetro titulo a la propiedad titulo
-        this.autor = autor;              // Asigna el valor del parámetro autor a la propiedad autor
-        this.numeroPaginas = numeroPaginas; // Asigna el valor del parámetro numeroPaginas a la propiedad numeroPaginas
+        this.titulo = titulo;            
+        this.autor = autor;              
+        this.numeroPaginas = numeroPaginas; 
     }
 
     // Método para mostrar los detalles del libro
     public void mostrarDetalles() {
-        System.out.println("Titulo: " + this.titulo); // Muestra el título del libro
-        System.out.println("Autor: " + this.autor);   // Muestra el autor del libro
-        System.out.println("Número de páginas: " + this.numeroPaginas); // Muestra el número de páginas del libro
-    }
+        System.out.println("Titulo: " + this.titulo); 
+        System.out.println("Autor: " + this.autor);  
+        System.out.println("Número de páginas: " + this.numeroPaginas); 
 
     // Método principal para probar la clase
     public static void main(String[] args) {
-        // Crear un objeto de Libro usando el constructor por defecto
+        
         Libro libro1 = new Libro();
-        libro1.mostrarDetalles(); // Llama al método para mostrar los detalles del libro
+        libro1.mostrarDetalles(); 
 
-        // Crear un objeto de Libro usando el constructor parametrizado
+        
         Libro libro2 = new Libro("1984", "George Orwell", 328);
-        libro2.mostrarDetalles(); // Llama al método para mostrar los detalles del libro
-    }
+        libro2.mostrarDetalles(); 
 }
